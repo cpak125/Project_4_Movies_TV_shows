9d813d731f1c683bf74a6d32dc410a30fd4d00aa
Chris Pak <chris.pak125@gmail.com>
2018-11-05 20:04:42 -0500

    deployed to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+
+chris = User.create(name: "Chris", age: 27, location:"Atlanta,GA")
