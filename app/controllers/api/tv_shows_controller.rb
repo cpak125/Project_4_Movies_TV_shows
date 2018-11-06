0d392bce4d04e6f57e02c0023f7c0443ddd5efba
Chris Pak <chris.pak125@gmail.com>
2018-11-05 21:40:36 -0500

    Show all, show one, create, and delete for Tv Shows controller works

--- app/controllers/api/tv_shows_controller.rb
+++ app/controllers/api/tv_shows_controller.rb
@@ -1,2 +1,32 @@
 class Api::TvShowsController < ApplicationController
+
+def index
+    @user = User.find(params[:user_id])
+    @tv_shows = @user.tv_shows
+    render json: @tv_shows
+end
+
+def show
+    @tv_show = TvShow.find(params[:id])
+    @user = @tv_show.user
+    render json: @tv_show
+end
+
+def create
+    @user = User.find(params[:user_id])
+    @tv_show = @user.tv_shows.create!(tv_show_params)
+    @tv_shows = @user.tv_shows
+    render json: @tv_shows
+end
+
+def destroy
+    @tv_show = TvShow.find(params[:id]).destroy
+    render json: 200
+end
+
+private
+def tv_show_params
+    params.require(:tv_show).permit(:title, :genre, :first_air_date, :overview, :networks, :number_of_seasons, :number_of_episodes, :status, :img_url)
+end
+
 end
