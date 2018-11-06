a3662e090e065150037d51188a915a133e852a8c
Chris Pak <chris.pak125@gmail.com>
2018-11-05 20:22:41 -0500

    Full CRUD for Users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,33 @@
 class Api::UsersController < ApplicationController
+
+def index
+    @users = User.all
+    render json: @users
+end
+
+def show
+    @user = User.find(params[:id])
+    render json: @user
+end
+
+def create
+    @user = User.create!(user_params)
+    render json: @user
+end
+
+def destroy
+    @user = User.find(params[:id]).destroy
+    render status: 200
+end
+
+def update
+    @user = User.find(params[:id]).update(user_params)
+    render json: @user
+end
+
+private
+def user_params
+    params.require(:user).permit(:name, :age, :location)
+end
+
 end
