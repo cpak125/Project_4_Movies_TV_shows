53ecac7e188fc60d4b29d7cd8ed46b4641f0d3fc
Chris Pak <chris.pak125@gmail.com>
2018-11-13 13:18:29 -0500

    Styled Landing Page footer

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import { Button } from 'semantic-ui-react';
+import { Button, Image } from 'semantic-ui-react';
 
 const Page = styled.div`
 background-image:url('https://i.imgur.com/NQk1Lcq.jpg');
@@ -44,14 +44,42 @@ margin-top:10px;
 }
 `
 
+const StyledLogo = styled(Image)`
+&&&{
+  width:8vw;
+  height:15vh;
+  float:right;
+}
+`
+const StyledFooter = styled.div`
+  background-color: rgba(0,0,0, .6);
+  position:fixed;
+  bottom: 0px;
+  margin: 0 auto;
+  width:100vw;
+  p{
+    font-size:12px;
+    color:#02c39a;
+    position:fixed;
+    bottom:0px;
+    margin-left:25vw;
+  }
+`
+
 export default class LandingPage extends Component {
   render() {
     return (
-        <Page>
-          <h1>Watcher</h1>
-          <p>Welcome to Watcher! This is an easy-to-use app that allows you to share your favorite movies and TV shows with people around the world. You can start off by creating your own personal profile and then search for movies and tv shows to add to your favorites list. Or you can view other peoples' favorite movies and tv shows by checking out their profile. Click "Enter" below to get started.  </p>
-          <Link to='/users'><StyledButton>Enter</StyledButton></Link>
-        </Page>
+
+      <Page>
+        <h1>Watcher</h1>
+        <p>Welcome to Watcher! This is an easy-to-use app that allows you to share your favorite movies and TV shows with people around the world. You can start off by creating your own personal profile and then search for movies and tv shows to add to your favorites list. Or you can view other peoples' favorite movies and tv shows by checking out their profile. Click "Enter" below to get started.  </p>
+        <Link to='/users'><StyledButton>Enter</StyledButton></Link>
+        <StyledFooter>
+          <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+          <StyledLogo floated='right' src='https://www.themoviedb.org/assets/1/v4/logos/293x302-powered-by-square-green-3ee4814bb59d8260d51efdd7c124383540fc04ca27d23eaea3a8c87bfa0f388d.png' alt='TMDB logo'>
+          </StyledLogo>
+        </StyledFooter>
+      </Page>
     )
   }
 }
