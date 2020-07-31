869f0430862f60e492c57043f69364ec76a5603c
Chris Pak <chris.pak125@gmail.com>
2020-07-30 21:09:05 -0400

    updated dependencies

---
+++ public/precache-manifest.301a2f3e115f2d22eab2fd126125685c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e287f77d2786a9db94043a4a587c49e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49c715da0f6898373059",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "53c9effd83d38cca923a",
+    "url": "/static/js/2.a92af8cb.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/static/js/2.a92af8cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49c715da0f6898373059",
+    "url": "/static/js/main.8ece2106.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
